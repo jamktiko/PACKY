513ee0ef40eafbf5bf854d9bde0e422d8c2067d6
Petri Paasila <ad1782@student.jamk.fi>
2024-09-05 14:34:51 +0300

    GridButton imported to grid and modal.tsx file created

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import GridButton from '../buttons/GridButton';
 
 //functional component grid, creating an array with 9x9 buttons
 const Grid: React.FC = () => {
   const cells = Array.from({ length: 81 }, (_, i) => (
-    <button key={i} onClick={() => console.log(i)}>
+    <GridButton key={i} onClick={() => console.log(i)}>
       {i}
-    </button>
+    </GridButton>
   ));
 
   return <div className="grid grid-cols-9 gap-4">{cells}</div>;
