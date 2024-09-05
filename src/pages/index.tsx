2384957cabc6e90dc03810ea08d6321fca45dd60
AatuMaenpaa <AD1741@student.jamk.fi>
2024-09-05 13:19:25 +0300

    Buttons added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,12 @@
 import GridButton from '@/components/buttons/GridButton';
 import type { PageLayout } from './_app';
 
-<<<<<<< HEAD
 const Page: PageLayout = () => {
   return (
     <div>
       <GridButton onClick={() => console.log('Clicked')}></GridButton>
     </div>
   );
-=======
-const Index: PageLayout = () => {
-  return <p className=''>asdasd</p>;
->>>>>>> 473d510b1ecfbaed48c4816fb1ab4ba63428b330
 };
 
-export default Index;
+export default Page;
