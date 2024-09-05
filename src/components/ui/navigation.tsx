06a19cd82ddb7a5f2760ce1bc341a0c631c1a503
unknown <augustboesen@gmail.com>
2024-09-05 13:29:07 +0300

    navigation update

--- src/components/ui/navigation.tsx
+++ src/components/ui/navigation.tsx
@@ -7,33 +7,42 @@ import {
   IoMailOutline,
 } from 'react-icons/io5';
 
+import Link from 'next/link';
+
 const Navigation = () => {
   return (
     <nav>
       <ul>
         <li>
-          <IoHome className='text-2xl ml-2 mr-4' />
-          Index
+          <Link href='/'>
+            {' '}
+            <IoHome className='text-2xl ml-2 mr-4' />
+            <span>Index</span>
+          </Link>
         </li>
         <li>
           {' '}
-          <IoGridOutline className='text-2xl ml-2 mr-4' /> StackBuilder
+          <IoGridOutline className='text-2xl ml-2 mr-4' />{' '}
+          <span>StackBuilder</span>
         </li>
         <li>
           {' '}
-          <IoLibraryOutline className='text-2xl ml-2 mr-4' /> Library
+          <IoLibraryOutline className='text-2xl ml-2 mr-4' />{' '}
+          <span>Library</span>
         </li>
         <li>
           {' '}
-          <IoGitCompareOutline className='text-2xl ml-2 mr-4' /> Compare
+          <IoGitCompareOutline className='text-2xl ml-2 mr-4' />{' '}
+          <span>Compare</span>
         </li>
         <li>
           {' '}
-          <IoHelpCircleOutline className='text-2xl ml-2 mr-4' /> About
+          <IoHelpCircleOutline className='text-2xl ml-2 mr-4' />{' '}
+          <span>About</span>
         </li>
         <li>
           {' '}
-          <IoMailOutline className='text-2xl ml-2 mr-4' /> Contact
+          <IoMailOutline className='text-2xl ml-2 mr-4' /> <span>Contact</span>
         </li>
       </ul>
     </nav>
