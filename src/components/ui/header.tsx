991a320dca122dd0228c481a9c8f5ee0e8340fef
unknown <augustboesen@gmail.com>
2024-09-09 13:28:05 +0300

    navigation base complete

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,20 +1,31 @@
 import Navigation from './navigation';
 import { useState } from 'react';
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
 
 const Header = () => {
   const [open, setOpen] = useState(false);
   return (
     <header>
-      <Image src={'/packyicon1.png'} width={100} height={100} alt={'packy'} />
+      <Image src={'/packyicon2.png'} width={100} height={100} alt={'packy'} />
       <h1 className='absolute text-start left-16 top-2 blur opacity-70'>
         PACKY
       </h1>
       <h1>PACKY</h1>
       <button onClick={() => setOpen(!open)}>☰</button>
       <AnimatePresence>
-        {open && <Navigation onLinkClick={() => setOpen(false)} />}
+        {open && (
+          <>
+            <Navigation onLinkClick={() => setOpen(false)} />
+            <motion.div
+              onClick={() => setOpen(false)}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className='absolute w-screen h-screen top-16 bg-black bg-opacity-50 backdrop-blur-sm z-10'
+            />
+          </>
+        )}
       </AnimatePresence>
     </header>
   );
--- src/components/ui/navigation.tsx
+++ src/components/ui/navigation.tsx
@@ -19,9 +19,9 @@ interface NavigationProps {
 const Navigation: React.FC<NavigationProps> = ({ onLinkClick }) => {
   return (
     <motion.nav
-      initial={{ opacity: 0, x: -200 }}
+      initial={{ opacity: 0, x: 200 }}
       animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: -200 }}
+      exit={{ opacity: 0, x: 200 }}
     >
       <ul>
         <li>
