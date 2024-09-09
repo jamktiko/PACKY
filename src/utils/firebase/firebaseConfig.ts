3b288264a0c20aa7fd577d0e782c30fc2fc4dc93
Arttu Henriksson <arttu.henriksson@student.jamk.fi>
2024-09-09 10:59:53 +0300

    firebaseconfig.ts fixed

--- src/utils/firebase/firebaseConfig.ts
+++ src/utils/firebase/firebaseConfig.ts
@@ -1,27 +1,7 @@
 import { initializeApp, getApps } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-// Export environment variables for configuration
-export const API_KEY = process.env.FIREBASE_API_KEY;
-export const AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN;
-export const PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
-export const STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
-export const MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID;
-export const APP_ID = process.env.FIREBASE_APP_ID;
+import { getFirestore } from 'firebase/firestore';
 
-// Checking for missing Firebase credentials
-if (
-  !API_KEY ||
-  !AUTH_DOMAIN ||
-  !PROJECT_ID ||
-  !STORAGE_BUCKET ||
-  !MESSAGING_SENDER_ID ||
-  !APP_ID
-) {
-  throw new Error('Missing Firebase credentials');
-}
-
-// Creating firebaseConfig object for firebase
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -31,12 +11,7 @@ const firebaseConfig = {
   appId: process.env.FIREBASE_APP_ID,
 };
 
-// Initialize the Firebase app if it hasn't been initialized already
 export let firebase_app =
   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-// Exporting firestore
 export const db = getFirestore(firebase_app);
-
-// Exporting auth
-export const auth = getAuth(firebase_app);
