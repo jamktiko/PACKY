b318e443eebbb864f43e2aa2f1ce5aed2f230db7
Arttu Henriksson <arttu.henriksson@student.jamk.fi>
2024-09-09 09:58:05 +0300

    added comments

--- src/utils/firebase/firebaseConfig.ts
+++ src/utils/firebase/firebaseConfig.ts
@@ -1,9 +1,11 @@
-// firebaseConfig.js
-
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+/**
+ * firebaseConfig object is for configuring the firebase app
+ * The values are retrieved from environment variables
+ */
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -13,7 +15,11 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+// Initialize Firebase with the given config
 const app = initializeApp(firebaseConfig);
 
+/**
+ * Exports the auth and firestore instances
+ */
 export const auth = getAuth(app);
 export const db = getFirestore(app);
