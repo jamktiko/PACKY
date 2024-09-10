da09bf48d0932e02bb601ef3a990105221202f12
Matias <matias01ju@gmail.com>
2024-09-10 13:57:02 +0300

    Added comments to grid.tsx

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -13,15 +13,18 @@ const Grid: React.FC<GridProps> = ({ setIsModalOpen }) => {
   const gridSize = 9;
   const [activeCells, setActiveCells] = useState<
     { row: number; col: number }[]
+    // Changes cells to active state
   >([{ row: Math.floor(gridSize / 2), col: Math.floor(gridSize / 2) }]);
   const [choosableCells, setChoosableCells] = useState<
     { row: number; col: number }[]
   >([]);
-
+  //changes cells to choosable state
   useEffect(() => {
+    // This is a hook that updates the grid states
     const updateGridStates = () => {
       const newChoosableCells: { row: number; col: number }[] = [];
-
+      // Gets all neighboring cells that are not active
+      // so they can be updated to choosable state
       activeCells.forEach(({ row, col }) => {
         const neighbors = [
           { row: row - 1, col: col }, // Up
@@ -29,7 +32,9 @@ const Grid: React.FC<GridProps> = ({ setIsModalOpen }) => {
           { row: row, col: col - 1 }, // Left
           { row: row, col: col + 1 }, // Right
         ];
-
+        // checks that the neighbors are not out of bounds
+        // if they are within the boundries of the grid
+        // they will be added to the newChoosableCells array
         neighbors.forEach(({ row: nRow, col: nCol }) => {
           if (nRow >= 0 && nRow < gridSize && nCol >= 0 && nCol < gridSize) {
             const isActive = activeCells.some(
@@ -41,7 +46,7 @@ const Grid: React.FC<GridProps> = ({ setIsModalOpen }) => {
           }
         });
       });
-
+      // sets the newChoosableCells array to the choosableCells array
       setChoosableCells(newChoosableCells);
     };
 
@@ -49,11 +54,14 @@ const Grid: React.FC<GridProps> = ({ setIsModalOpen }) => {
   }, [activeCells]);
 
   const handleGridButtonClick = (row: number, col: number) => {
+    // Opens the modal when that cell is clicked
     setIsModalOpen(true);
+    // Adds the clicked cell to the activeCells array
     setActiveCells((prevActiveCells) => [...prevActiveCells, { row, col }]);
   };
 
   return (
+    // Grid component is constructed here
     <div
       className="grid grid-cols-9 gap-4"
       style={{
@@ -61,6 +69,7 @@ const Grid: React.FC<GridProps> = ({ setIsModalOpen }) => {
         gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
       }}
     >
+      {/* Grid buttons are constructed here */}
       {Array.from({ length: gridSize * gridSize }, (_, index) => {
         const row = Math.floor(index / gridSize);
         const col = index % gridSize;
@@ -73,6 +82,7 @@ const Grid: React.FC<GridProps> = ({ setIsModalOpen }) => {
         const opacity = isActive || isChoosable ? 1 : 0.5; // Adjust opacity based on state
 
         return (
+          // Renders the grid buttons
           <GridButton
             key={`${row}-${col}`}
             row={row}
