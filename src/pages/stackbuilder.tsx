ff7d584fe1b52bb8264d6563ab09f8fa97c135b3
Petri Paasila <ad1782@student.jamk.fi>
2024-09-10 13:00:43 +0300

    state to control GridModal created in stackbuilder and shared with GridModal andgrid

--- src/pages/stackbuilder.tsx
+++ src/pages/stackbuilder.tsx
@@ -1,8 +1,23 @@
 import type { PageLayout } from './_app';
 import Grid from '../components/grid/grid';
+import GridModal from '@/components/modals/GridModal';
+import { useState } from 'react';
 
+// StackBuilder page is constructed here, it renders Grid component and conditionally renders GridModal
 const StackBuilder: PageLayout = () => {
-  return <Grid></Grid>;
+  // useState hook manages a boolean state that controls wheter or not the modal is open and a function to update it
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  /*const [selectedButtonData, setSelectedButtonData] = useState({ id: '' });*/
+
+  // Grid component and GridModal are rendered here
+  // setIsModalOpen is passed as a prop to Grid and GridModal, to allow them to update the state
+  //of isModalOpen
+  return (
+    <>
+      <Grid setIsModalOpen={setIsModalOpen} />
+      {isModalOpen && <GridModal />}
+    </>
+  );
 };
 
 export default StackBuilder;
