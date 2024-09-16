ce16393c34e18efc036ccd86c5c4804f894bb4a1
Arttu Henriksson <ad1820@student.jamk.fi>
2024-09-16 13:34:17 +0300

    Comments comments

--- src/components/lists/list.tsx
+++ src/components/lists/list.tsx
@@ -3,28 +3,35 @@ import { Triangle } from 'react-loader-spinner';
 import { CollectionData } from '@/utils/collectionData';
 import { getAllCollections } from '@/utils/firebase/firebaseService';
 /**
- * THIS FILE IS GOING TO BE USED FOR THE LIBRARY PAGE AND IT WILL BE DIFFERENT THIS IS JUST THE DEMO
+ * THIS COMPONENT IS GOING TO BE USED FOR THE LIBRARY PAGE
  */
 
 const List = () => {
+  // Initialize state with default values and empty array
   const [collectionName, setCollectionName] = useState('frontendFrameworks');
-  const [data, setData] = useState<CollectionData[]>([]);
+  const [data, setData] = useState<CollectionData[]>([]); // Data fetched from Firebase
+  const [isLoading, setIsLoading] = useState(true); // Loading state
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await getAllCollections(collectionName);
-      setData(data);
+      const data = await getAllCollections(collectionName); // Fetch data from Firebase
+      setData(data); // Updating the state with fetched data
+      setTimeout(() => {
+        setIsLoading(false); // Set loading state to false after one second
+      }, 1000);
     };
-    fetchData();
+    fetchData(); // Fetch data when component mounts or collectionName changes
   }, [collectionName]);
 
   const handleCollectionChange = (
     event: React.ChangeEvent<HTMLSelectElement>
   ) => {
-    setCollectionName(event.target.value);
+    setCollectionName(event.target.value); // Updated the collections name
+    setIsLoading(true); // set loading state to true
   };
   return (
     <>
+      {/* Select input to choose collection */}
       <select
         value={collectionName}
         onChange={handleCollectionChange}
@@ -36,35 +43,39 @@ const List = () => {
         <option value="databases">Databases</option>
         <option value="cloudservices">Cloudservices</option>
         <option value="apis">Apis</option>
-        <option value="features">Features</option>
         <option value="languages">Languages</option>
         <option value="libraries">Libraries</option>
-        <option value="other_tools">Other toolss</option>
+        <option value="other_tools">Other tools</option>
       </select>
-      <div className="content">
-        {data.map((item) => (
-          <div key={item.id}>
-            <h1>{item.name}</h1>
-            <p>{item.description}</p>
-          </div>
-        ))}
-      </div>
-      {/* <div className="mt-4 text-center">
-        <p className="font-bold">Loading the library...</p>
-        <Triangle
-          visible={true}
-          height="100"
-          width="100"
-          color="#38B2AC"
-          ariaLabel="triangle-loading"
-          wrapperStyle={{
-            width: '100%',
-            position: 'flex',
-            justifyContent: 'center',
-          }}
-          wrapperClass=""
-        />
-      </div> */}
+      {/* Loading spinner */}
+      {isLoading ? (
+        <div className="mt-4 text-center">
+          <p className="font-bold">Loading the library...</p>
+          <Triangle
+            visible={true}
+            height="100"
+            width="100"
+            color="#38B2AC"
+            ariaLabel="triangle-loading"
+            wrapperStyle={{
+              width: '100%',
+              position: 'flex',
+              justifyContent: 'center',
+            }}
+            wrapperClass=""
+          />
+        </div>
+      ) : (
+        // Render data in a list
+        <div className="content">
+          {data.map((item) => (
+            <div key={item.id}>
+              <h1>{item.name}</h1>
+              <p>{item.description}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 };
--- src/utils/firebase/firebaseService.ts
+++ src/utils/firebase/firebaseService.ts
@@ -4,16 +4,19 @@ import { CollectionData } from '@/utils/collectionData';
 
 export const getAllCollections = async (collectionName: string) => {
   try {
+    // Reference to the specific collection
     const collectionReference = collection(db, collectionName);
+    // Get all documents from the collection
     const snapshot = await getDocs(collectionReference);
+    // Map the documents to an array of CollectionData objects
     const data = snapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
+    // Returning the data
     return data as CollectionData[];
   } catch (error) {
     console.error(`Error retrieving data from ${collectionName}: ${error}`);
-
     throw error;
   }
 };
