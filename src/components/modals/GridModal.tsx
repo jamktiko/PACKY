72b8bd0768d799f2f73d906ae72e983cbe604527
unknown <augustboesen@gmail.com>
2024-09-16 13:23:52 +0300

    gridmodal hakee dataa onnistuneesti

--- src/components/modals/GridModal.tsx
+++ src/components/modals/GridModal.tsx
@@ -13,29 +13,45 @@ MUISTA KATSOA ETTÄ OLET OIKEALLA BRANCHILLA ENNEN KUIN ALAT TEKEMÄÄN ASIOITA 
 
 */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { toggleModal } from '@/redux/reducers/modalReducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, type RootState } from '@/redux/store/store';
-import fetchData from '@/redux/actions/dataActions';
-import { CollectionData } from '@/utils/collectionData';
+import { fetchCollections } from '@/redux/reducers/dataReducer';
 
 const GridModal = () => {
+  // Haetaan modalin tila Redux-storesta
   const modal = useSelector((state: RootState) => state.modalReducer.value);
+
+  // Määritellään dispatch-funktio, jota käytetään Redux-toimintojen kutsuun
   const dispatch = useDispatch<AppDispatch>();
 
-  const modalData = useSelector((state: RootState) => state.dataReducer.value);
-  const dispatchData = useDispatch<AppDispatch>();
+  // Käytetään useEffect-hookia, joka suoritetaan aina kun komponentti renderöidään
+  useEffect(() => {
+    // Haetaan käyttäjän kokoelmat Redux-storesta
+    dispatch(fetchCollections());
+  }, [dispatch]); // Tämä efektin riippuvuuslista tarkoittaa, että efektin suoritus uudelleen, kun dispatch-funktio muuttuu
+
+  // Haetaan käyttäjän data Redux-storesta
+  const data = useSelector((state: RootState) => state.dataReducer.value);
+
+  // Suodatetaan data, jossa on nimi
+  const modalData = data.filter((item) => item.name);
   return (
     <>
       {modal && (
-        <div className="z-50 absolute top-0 left-0 w-screen h-screen bg-red-500 opacity-50">
-          <h1>Tähän data</h1>
-          <h1>{modalData}</h1>
+        <div className='z-50 absolute top-0 left-0 w-screen h-screen bg-black backdrop-blur-sm bg-opacity-50'>
+          <h1 className='bg-blue-500'>Tähän data</h1>
+          {modalData.map((item, id) => (
+            <div key={id} className='bg-green-500'>
+              <h1>{item.name}</h1>
+              <p>{item.description}</p>
+            </div>
+          ))}
           <button
-            className="bg-white p-2 rounded-md"
+            className='bg-white p-2 rounded-md'
             onClick={() => dispatch(toggleModal(false))}
-            type="button"
+            type='button'
           >
             Close
           </button>
