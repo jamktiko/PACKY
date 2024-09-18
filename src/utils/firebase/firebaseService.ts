a3a390dcfc75749612b868686459a2e24b435759
Arttu Henriksson <ad1820@student.jamk.fi>
2024-09-18 12:41:56 +0300

    New firebase method for getting docs by tags

--- src/utils/firebase/firebaseService.ts
+++ src/utils/firebase/firebaseService.ts
@@ -1,4 +1,4 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from './firebaseConfig';
 import { CollectionData } from '@/utils/collectionData';
 
@@ -19,4 +19,37 @@ export const getAllCollections = async (collectionName: string) => {
     console.error(`Error retrieving data from ${collectionName}: ${error}`);
     throw error;
   }
+};
+
+export const getDocumentsByTags = async (tags: {
+  name: string;
+  tags: string[];
+}) => {
+  // The names of the collections to query
+  const collections = [
+    'databases',
+    'backendFrameworks',
+    'frontendFrameworks',
+    'languages',
+    'apis',
+    'cloudservices',
+  ];
+  // Initialize an empty array to store the documents
+  const docs: CollectionData[] = [];
+  // Looping throigh the collections
+  for (const collName of collections) {
+    // Creating query to search for documents with the given tags
+    const q = query(
+      collection(db, collName),
+      where('tags', 'array-contains-any', tags)
+    );
+    // Get all documents that match the query
+    const snapshot = await getDocs(q);
+    // Looping through the documents
+    snapshot.forEach((doc) => {
+      // Adding the document to the array
+      docs.push({ id: doc.id, ...doc.data() } as CollectionData);
+    });
+  }
+  return docs;
 };
