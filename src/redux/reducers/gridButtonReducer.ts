4d1b86e785a50f29824b6536382f0f0e01ff2bcc
Petri Paasila <ad1782@student.jamk.fi>
2024-09-18 13:44:12 +0300

    gridButtonReducer modified to create two arrays

--- src/redux/reducers/gridButtonReducer.ts
+++ src/redux/reducers/gridButtonReducer.ts
@@ -1,13 +1,17 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 //interface Item is created
+//item is the data that is meant for buttons
+//wholeItem is the data that is meant for outputModal/list
 interface Item {
   item: { name: string; tags: string[] }[];
+  wholeItem: { name: string; tags: string[]; description: string }[];
 }
 
 //initialState is created and given Item as a type
 const initialState: Item = {
   item: [],
+  wholeItem: [],
 };
 
 //Slice created from initialState and reducer function addItem created
@@ -16,8 +20,23 @@ const itemSlice = createSlice({
   name: 'item',
   initialState,
   reducers: {
-    addItem(state, action: PayloadAction<{ name: string; tags: string[] }>) {
-      state.item.push({ name: action.payload.name, tags: action.payload.tags });
+    addItem(
+      state,
+      action: PayloadAction<{
+        name: string;
+        tags: string[];
+        description: string;
+      }>
+    ) {
+      state.item.push({
+        name: action.payload.name,
+        tags: action.payload.tags,
+      });
+      state.wholeItem.push({
+        name: action.payload.name,
+        tags: action.payload.tags,
+        description: action.payload.description,
+      });
     },
   },
 });
