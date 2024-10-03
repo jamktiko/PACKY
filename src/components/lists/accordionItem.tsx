e80e58aa86c7bb8adeb205476f93f262a3212f72
unknown <augustboesen@gmail.com>
2024-10-03 11:02:42 +0300

    output modal styling

--- src/components/lists/accordionItem.tsx
+++ src/components/lists/accordionItem.tsx
@@ -1,65 +1,108 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useOutputFetch } from '@/hooks/outputFetch';
 import React, { useState } from 'react';
 
-// Define the AccordionProps interface with two properties
 interface AccordionProps {
   title: string;
   description: { [key: string]: string[] };
 }
 
 export const AccordionItem = ({ title, description }: AccordionProps) => {
-  console.log('description:', description);
-  // Initialize the isOpen state variable to false
   const [isOpen, setIsOpen] = useState(false);
-  // Define the handleToggle function to toggle the isOpen state variable
+
   const handleToggle = () => {
     setIsOpen(!isOpen);
   };
 
-  // Define the descriptionText variable by transforming the description object into a string
   const descriptionText = description
-    ? Object.entries(description)
-        .map(([key, value]) => {
-          const techs = value.join(' | ');
-          let sentence = '';
-          switch (key) {
-            case 'frontendFramework':
-              sentence = `For the frontend, we recommend these frameworks:  ${techs}.`;
-              break;
-            case 'backendFramework':
-              sentence = `We suggest using this framework for the backend: ${techs}.`;
-              break;
-            case 'api':
-              sentence = `For this feature, consider these API's: ${techs}.`;
-              break;
-            case 'library':
-              sentence = `You might find these libraries useful: ${techs}.`;
-              break;
-            case 'service':
-              sentence = `We suggest using these services: ${techs}.`;
-              break;
-            case 'database':
-              sentence = `We suggest using these databases: ${techs}.`;
-              break;
-            case 'Language':
-              sentence = `For this feature, consider these languages: ${techs}.`;
-              break;
-          }
-          return sentence;
-        })
-        .filter(Boolean) // Filter out any falsey values
-        .join(' ') // Join the sentences into a single string
-    : '';
+    ? Object.entries(description).flatMap(([key, values]) => {
+        let sentence: React.ReactNode;
+        switch (key) {
+          case 'frontendFramework':
+            sentence = (
+              <>
+                For the frontend, we recommend these frameworks:{' '}
+                <span className='text-orange-500 font-normal bg-white bg-opacity-5 rounded-2xl text-center block'>
+                  {values.join(', ')}
+                </span>
+              </>
+            );
+            break;
+          case 'backendFramework':
+            sentence = (
+              <>
+                We suggest using this framework for the backend:{' '}
+                <span className='text-cyan-500 font-normal bg-white bg-opacity-5 rounded-2xl text-center block'>
+                  {values.join(', ')}
+                </span>
+              </>
+            );
+            break;
+          case 'api':
+            sentence = (
+              <>
+                For this feature, consider these API's:{' '}
+                <span className='text-lime-500 font-normal bg-white bg-opacity-5 rounded-2xl text-center block'>
+                  {values.join(', ')}
+                </span>
+              </>
+            );
+            break;
+          case 'library':
+            sentence = (
+              <>
+                You might find these libraries useful:{' '}
+                <span className='text-teal-500 font-normal bg-white bg-opacity-5 rounded-2xl text-center block'>
+                  {values.join(', ')}
+                </span>
+              </>
+            );
+            break;
+          case 'service':
+            sentence = (
+              <>
+                We suggest using these services:{' '}
+                <span className='text-fuchsia-500 font-normal bg-white bg-opacity-5 rounded-2xl text-center block'>
+                  {values.join(', ')}
+                </span>
+              </>
+            );
+            break;
+          case 'database':
+            sentence = (
+              <>
+                We suggest using these databases:{' '}
+                <span className='text-purple-500 font-normal bg-white bg-opacity-5 rounded-2xl text-center block'>
+                  {values.join(', ')}
+                </span>
+              </>
+            );
+        }
+        return [sentence];
+      })
+    : null;
 
   return (
-    <div className="bg-slate-500 px-6 bg-opacity-50 w-96 border rounded-xl">
-      <button className="w-full flex justify-between" onClick={handleToggle}>
-        <p>{title}</p>
-        <p className="bg-slate-900 w-10 h-full rounded-xl">
+    <div className='my-6 bg-slate-500 bg-opacity-50 w-96 border border-teal-500 rounded-xl'>
+      <button
+        className='w-full flex justify-between border-b border-inherit bg-black bg-opacity-50'
+        onClick={handleToggle}
+      >
+        <p className='pl-4'>{title}</p>
+        <p className='bg-black border border-inherit bg-opacity-50 w-10 h-full rounded-xl'>
           {isOpen ? '-' : '+'}
         </p>
       </button>
-      {isOpen && <div className="text-base text-left">{descriptionText}</div>}
+      {isOpen && (
+        <div className='text-base text-left px-4'>
+          {isOpen &&
+            descriptionText?.map((sentence, index) => (
+              <p className='border-y my-1 font-bold' key={index}>
+                {sentence}
+              </p>
+            ))}
+        </div>
+      )}
     </div>
   );
 };
