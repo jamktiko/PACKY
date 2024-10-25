9efa5eb16706df90ddebdc86c1954dc019f39d5e
Petri Paasila <ad1782@student.jamk.fi>
2024-10-25 11:36:28 +0300

    added disable functionality for buttons

--- src/pages/stackbuilder.tsx
+++ src/pages/stackbuilder.tsx
@@ -2,10 +2,10 @@ import type { PageLayout } from './_app';
 import Grid from '../components/grid/grid';
 import GridModal from '@/components/modals/GridModal';
 import OutputModal from '@/components/modals/OutputModal';
-import { useState } from 'react';
+import { act, useCallback, useEffect, useState } from 'react';
 import Head from 'next/head';
 import { toggleOutputModal } from '@/redux/reducers/outputReducer';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import List from '@/components/lists/librarylist';
 import Library from './library';
 import { resetGridState } from '@/redux/reducers/gridStateReducer';
@@ -13,8 +13,14 @@ import { IoArrowUndoOutline } from 'react-icons/io5';
 import { IoIosArrowDropright } from 'react-icons/io';
 import { GrPowerReset } from 'react-icons/gr';
 import { FaCheck } from 'react-icons/fa';
+import { RootState } from '@/redux/store/store';
 // StackBuilder page is constructed here, it renders Grid component and conditionally renders GridModal
 const StackBuilder: PageLayout = () => {
+  // Get the activeCells from the Redux state
+  const { activeCells } = useSelector(
+    (state: RootState) => state.gridStateReducer
+  );
+
   // useState hook manages a boolean state that controls wheter or not the modal is open and a function to update it
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isLibraryOpen, setIsLibraryOpen] = useState(true);
@@ -37,6 +43,20 @@ const StackBuilder: PageLayout = () => {
     setIsGridOpen(!isGridOpen);
   };
 
+  //Function to handle button disablement
+  const buttonDisabled = useCallback(() => {
+    if (activeCells.length > 1) {
+      return false;
+    } else {
+      return true;
+    }
+  }, [activeCells]);
+
+  //UseEffect hook to handle button disablement
+  useEffect(() => {
+    buttonDisabled();
+  }, [buttonDisabled, activeCells]);
+
   return (
     <>
       {/* _____________________________ */}
@@ -48,24 +68,25 @@ const StackBuilder: PageLayout = () => {
       {isLibraryOpen && (
         <>
           <Library />{' '}
-          <div className='w-full flex justify-center h-16 fixed bottom-0'></div>
-          <div className='h-16 fixed w-full bottom-0 flex justify-evenly'>
-            <button className='toggle-output pointer-events-none hover:bg-slate-500 hover:opacity-80 hover:text-slate-400 cursor-default text-slate-400 bg-slate-500'>
-              <IoArrowUndoOutline className='w-8 h-8' />
+          <div className="w-full flex justify-center h-16 fixed bottom-0"></div>
+          <div className="h-16 fixed w-full bottom-0 flex justify-evenly">
+            <button className="toggle-output pointer-events-none hover:bg-slate-500 hover:opacity-80 hover:text-slate-400 cursor-default text-slate-400 bg-slate-500">
+              <IoArrowUndoOutline className="w-8 h-8" />
               Go back
             </button>
             <button
-              className='toggle-output bg-gradient-to-r from-teal-500 to-cyan-500'
+              className="toggle-output bg-gradient-to-r from-teal-500 to-cyan-500"
               onClick={handlesetLibraryOpen}
             >
-              <IoIosArrowDropright className='w-8 h-8' />
+              <IoIosArrowDropright className="w-8 h-8" />
               Next
             </button>
+
             <button
-              className='toggle-output bg-cyan-500'
+              className="toggle-output bg-cyan-500"
               onClick={() => dispatch(resetGridState())}
             >
-              <GrPowerReset className='w-8 h-8' />
+              <GrPowerReset className="w-8 h-8" />
               Reset
             </button>
           </div>
@@ -74,26 +95,28 @@ const StackBuilder: PageLayout = () => {
       {isGridOpen && (
         <>
           <Grid setIsModalOpen={setIsModalOpen} />
-          <div className='h-16 fixed w-full bottom-0 flex justify-evenly'>
+          <div className="h-16 fixed w-full bottom-0 flex justify-evenly">
             <button
-              className='toggle-output bg-teal-500'
+              className="toggle-output bg-teal-500"
               onClick={handlesetLibraryOpen}
             >
-              <IoArrowUndoOutline className='w-8 h-8' />
+              <IoArrowUndoOutline className="w-8 h-8" />
               Go back
             </button>
             <button
-              className='toggle-output bg-gradient-to-r from-teal-500 to-cyan-500'
+              className="toggle-output bg-gradient-to-r from-teal-500 to-cyan-500"
               onClick={handlesetOutputModal}
+              disabled={buttonDisabled()}
             >
-              <FaCheck className='w-8 h-8' />
+              <FaCheck className="w-8 h-8" />
               Finish
             </button>
             <button
-              className='toggle-output right-0 z-50 bg-cyan-500'
+              className="toggle-output right-0 z-50 bg-cyan-500"
               onClick={() => dispatch(resetGridState())}
+              disabled={buttonDisabled()}
             >
-              <GrPowerReset className='w-8 h-8' />
+              <GrPowerReset className="w-8 h-8" />
               Reset
             </button>
           </div>
@@ -105,12 +128,12 @@ const StackBuilder: PageLayout = () => {
       {isOutputModalOpen && (
         <>
           <OutputModal />
-          <div className='h-16 z-50 fixed w-full bottom-0 flex justify-evenly'>
+          <div className="h-16 z-50 fixed w-full bottom-0 flex justify-evenly">
             <button
-              className='toggle-output min-w-96 bg-gradient-to-r from-teal-500 to-cyan-500'
+              className="toggle-output min-w-96 bg-gradient-to-r from-teal-500 to-cyan-500"
               onClick={handlesetOutputModal}
             >
-              <IoArrowUndoOutline className='w-8 h-8' />
+              <IoArrowUndoOutline className="w-8 h-8" />
               Go back
             </button>
           </div>
