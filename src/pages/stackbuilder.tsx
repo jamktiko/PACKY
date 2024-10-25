3cab27d1c0aed0b0ef52bb747bccc38e813e7d61
unknown <augustboesen@gmail.com>
2024-10-25 10:26:56 +0300

    added icons to buttons

--- src/pages/stackbuilder.tsx
+++ src/pages/stackbuilder.tsx
@@ -9,7 +9,10 @@ import { useDispatch } from 'react-redux';
 import List from '@/components/lists/librarylist';
 import Library from './library';
 import { resetGridState } from '@/redux/reducers/gridStateReducer';
-
+import { IoArrowUndoOutline } from 'react-icons/io5';
+import { IoIosArrowDropright } from 'react-icons/io';
+import { GrPowerReset } from 'react-icons/gr';
+import { FaCheck } from 'react-icons/fa';
 // StackBuilder page is constructed here, it renders Grid component and conditionally renders GridModal
 const StackBuilder: PageLayout = () => {
   // useState hook manages a boolean state that controls wheter or not the modal is open and a function to update it
@@ -47,14 +50,20 @@ const StackBuilder: PageLayout = () => {
           <Library />{' '}
           <div className='w-full flex justify-center h-16 fixed bottom-0'></div>
           <div className='h-16 fixed w-full bottom-0 flex justify-evenly'>
-            <button className='toggle-output' onClick={handlesetLibraryOpen}>
-              Next
+            <button className='toggle-output pointer-events-none cursor-default bg-slate-500'>
+              <IoArrowUndoOutline className='w-8 h-8' />
             </button>
             <button
-              className='toggle-output'
+              className='toggle-output bg-teal-500'
+              onClick={handlesetLibraryOpen}
+            >
+              <IoIosArrowDropright className='w-8 h-8' />
+            </button>
+            <button
+              className='toggle-output bg-cyan-500'
               onClick={() => dispatch(resetGridState())}
             >
-              Reset
+              <GrPowerReset className='w-8 h-8' />
             </button>
           </div>
         </>
@@ -63,17 +72,23 @@ const StackBuilder: PageLayout = () => {
         <>
           <Grid setIsModalOpen={setIsModalOpen} />
           <div className='h-16 fixed w-full bottom-0 flex justify-evenly'>
-            <button className='toggle-output' onClick={handlesetLibraryOpen}>
-              Return to Library
-            </button>
-            <button className='toggle-output' onClick={handlesetOutputModal}>
-              Finish
+            <button
+              className='toggle-output bg-teal-500'
+              onClick={handlesetLibraryOpen}
+            >
+              <IoArrowUndoOutline className='w-8 h-8' />
             </button>
             <button
-              className='toggle-output right-0 z-50'
+              className='toggle-output bg-teal-500'
+              onClick={handlesetOutputModal}
+            >
+              <FaCheck className='w-8 h-8' />
+            </button>
+            <button
+              className='toggle-output right-0 z-50 bg-cyan-500'
               onClick={() => dispatch(resetGridState())}
             >
-              Reset
+              <GrPowerReset className='w-8 h-8' />
             </button>
           </div>
         </>
@@ -85,8 +100,11 @@ const StackBuilder: PageLayout = () => {
         <>
           <OutputModal />
           <div className='h-16 z-50 fixed w-full bottom-0 flex justify-evenly'>
-            <button className='toggle-output' onClick={handlesetOutputModal}>
-              Return to Grid
+            <button
+              className='toggle-output min-w-96 bg-teal-500'
+              onClick={handlesetOutputModal}
+            >
+              <IoArrowUndoOutline className='w-8 h-8' />
             </button>
           </div>
         </>
