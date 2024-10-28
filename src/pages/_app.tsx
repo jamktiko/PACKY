717f4c625545c204bc8c2328c534a30c090918ab
unknown <augustboesen@gmail.com>
2024-10-28 13:17:18 +0200

    optimized _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
-import { motion } from 'framer-motion';
 import { useFetchCollections } from '@/hooks/useFetchCollections';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
@@ -12,11 +11,11 @@ import Header from '../components/ui/header';
 
 import '../assets/CSS/globals.css';
 import { Montserrat } from 'next/font/google';
-import Image from 'next/image';
-
+import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
+let persistor = persistStore(store);
 
 export type PageLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -26,41 +25,40 @@ type AppPropsWithLayout = AppProps & {
   Component: PageLayout;
 };
 
+const BackgroundImage = dynamic(() => import('next/image'), { ssr: false });
+
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
-  //älä poista
   useFetchCollections();
-  let persistor = persistStore(store);
 
-  // Layout is defined here
   return (
-    //entire app is wrapped with a Redux store provider
-    //created entrypoint to use persistor store with PersistGate method
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         {getLayout(
           <main className={montserrat.className}>
             <Head>
               <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+                name='viewport'
+                content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'
               />
             </Head>
             <Header />
-            <Image
+            <BackgroundImage
               src={'/bg.png'}
-              className=" blur-3xl fixed h-screen -z-50 overflow-hidden"
+              className='blur-3xl fixed h-screen -z-50 overflow-hidden'
               alt={'Background'}
               width={1920}
               height={1080}
+              priority={true}
             />
-            <div className="spin">
-              <Image
+            <div className='spin'>
+              <BackgroundImage
                 src={'/packyiconsmall.png'}
-                className="fixed -z-10 md:-left-[40rem] -left-48 md:-top-32 top-16 opacity-5"
+                className='fixed -z-10 md:-left-[40rem] -left-48 md:-top-32 top-16 opacity-5'
                 width={3200}
                 height={3200}
                 alt={'packy'}
+                priority={false}
               />
             </div>
 
