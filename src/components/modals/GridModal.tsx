79344f3acbbf4c4cdc001124c52c61fc2ee883b2
unknown <augustboesen@gmail.com>
2024-10-29 13:08:40 +0200

    added disabled to feature buttons and fixed styling

--- src/components/modals/GridModal.tsx
+++ src/components/modals/GridModal.tsx
@@ -1,27 +1,12 @@
-/*
-
-
-
-
-MUISTA KATSOA ETTÄ OLET OIKEALLA BRANCHILLA ENNEN KUIN ALAT TEKEMÄÄN ASIOITA SAATANA!!! 
-
-
-
-
-
-
-
-*/
-
-import React, { useState } from 'react';
+import React from 'react';
 import { toggleModal } from '@/redux/reducers/gridModalReducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, type RootState } from '@/redux/store/store';
-
 import { CollectionData } from '@/utils/collectionData';
 import { setActiveCells } from '@/redux/reducers/gridStateReducer';
 import { updateChoosableCells } from '@/utils/grid/updateGridState';
 import { motion } from 'framer-motion';
+
 interface Cell {
   row: number;
   col: number;
@@ -35,34 +20,20 @@ const GridModal = () => {
   const selectedCell = useSelector(
     (state: RootState) => state.gridStateReducer.selectedCell
   );
-
-  // Haetaan modalin tila Redux-storesta
   const gridmodal = useSelector(
     (state: RootState) => state.gridModalReducer.value
   );
-
-  // Määritellään dispatch-funktio, jota käytetään Redux-toimintojen kutsuun
   const dispatch = useDispatch<AppDispatch>();
-  // Haetaan käyttäjän data Redux-storesta
   const data = useSelector((state: RootState) => state.dataReducer.value);
 
-  // Suodatetaan data, jossa on nimi
-  //modified so that the data is put into an array and into a function to be able to use persist
   const gridmodalData = Array.isArray(data)
     ? data.filter((item) => item.name)
     : [];
 
-  console.log(gridmodalData);
-  //funktio handleclick kutsuu addItem funktiota, joka päivittää gridbuttonReduceriin
-  //tilan ja sulkeen modalin toggleModal funktiolla
   const handleClick = (item: CollectionData, description: string) => {
     if (activeCells.some((cell) => cell.id === item.name)) {
-      // Button has already been pressed, ilmoita käyttäjälle
       console.log(`Ominaisuus ${item.name} on jo valittu.`);
     } else {
-      // Lisätään item Redux-storeen vain, jos sitä ei ole vielä valittu
-      // päivitä button active-stateen
-      //filter out the selected cell from the active cells to update its state
       dispatch(
         setActiveCells([
           ...activeCells.filter(
@@ -80,7 +51,6 @@ const GridModal = () => {
       );
 
       updateChoosableCells(9);
-      // Suljetaan modaali
       dispatch(toggleModal(false));
     }
   };
@@ -93,16 +63,20 @@ const GridModal = () => {
           animate={{ opacity: 1, y: 0 }}
           className='grid-modal'
         >
-          {gridmodalData.map((item, id) => (
-            <button
-              key={id}
-              className='grid-modal-item'
-              onClick={() => handleClick(item, item.desc)}
-            >
-              <h1 className='font-bold'>{item.name}</h1>
-              <p>{item.desc}</p>
-            </button>
-          ))}
+          {gridmodalData.map((item, id) => {
+            const isActive = activeCells.some((cell) => cell.id === item.name);
+            return (
+              <button
+                key={id}
+                className={`grid-modal-item ${isActive ? 'active' : ''}`}
+                onClick={() => handleClick(item, item.desc)}
+                disabled={isActive}
+              >
+                <h1 className='font-bold'>{item.name}</h1>
+                <p>{item.desc}</p>
+              </button>
+            );
+          })}
           <div className='absolute top-0 left-0 z-50 w-screen text-3xl text-center py-2'>
             <h1>Choose feature</h1>
             <button
