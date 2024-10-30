19fb90357cb53d0339bf7e1adb3ad2d390a68fcd
unknown <augustboesen@gmail.com>
2024-10-30 10:16:59 +0200

    library styling update

--- src/components/buttons/ExpandField.tsx
+++ src/components/buttons/ExpandField.tsx
@@ -1,12 +1,11 @@
-// Create a separate component for the expandable item
-import React, { useState, useEffect } from 'react';
-import Image from 'next/image'; // Import Image component from Next.js
-import { SearchBarProps } from '../../utils/search'; // Import props from search to give data
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { SearchBarProps } from '../../utils/search';
 import Link from 'next/link';
-import { FaAngleUp, FaAngleDown } from 'react-icons/fa'; // Icons from react icons
+import { FaAngleUp } from 'react-icons/fa';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const ExpandableItem: React.FC<{ item: SearchBarProps }> = ({ item }) => {
-  // Checking that is expanded open or not and also can set open and close it. Starting with false
   const [isExpanded, setIsExpanded] = useState(false);
 
   const toggleExpand = () => {
@@ -14,8 +13,13 @@ const ExpandableItem: React.FC<{ item: SearchBarProps }> = ({ item }) => {
   };
 
   return (
-    <li onClick={toggleExpand} className='expand-container'>
-      <div className='flex justify-between'>
+    <motion.li
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className='flex flex-row' // Remove items-center to avoid vertical centering
+    >
+      <div onClick={toggleExpand} className='expand-container'>
         <div className='expand-info'>
           {item.image && (
             <Image
@@ -23,45 +27,46 @@ const ExpandableItem: React.FC<{ item: SearchBarProps }> = ({ item }) => {
               alt={item.name}
               width={40}
               height={30}
-              className='rounded-lg  w-16'
+              className='rounded-lg w-16'
               objectFit='cover'
             />
           )}
           <strong className='expand-header'>
             {item.link && (
-              <Link href={item.link} target='_blank' className=' expand-text'>
-                {item.name}{' '}
+              <Link href={item.link} target='_blank' className='expand-text'>
+                {item.name}
               </Link>
             )}
           </strong>
-        </div>
-        <div className='flex items-center'>
-          <button className='text-white font-bold hover:underline'>
-            {isExpanded ? (
-              <>
-                <FaAngleUp
-                  className='float-right'
-                  style={{ width: '30px', height: '30px' }}
-                />
-              </>
-            ) : (
-              <>
-                <FaAngleDown
-                  className='float-right'
-                  style={{ width: '30px', height: '30px' }}
-                />
-              </>
-            )}
+          <button className='text-white font-bold'>
+            <FaAngleUp
+              className={`float-right transition-transform ${
+                isExpanded ? 'rotate-180' : ''
+              }`}
+              style={{ width: '30px', height: '30px' }}
+            />
           </button>
-          <input
-            type='checkbox'
-            className='mr-2 rounded-full'
-            style={{ width: '30px', height: '30px' }}
-          />
         </div>
+        <div className='flex items-center'></div>
+        <AnimatePresence>
+          {isExpanded && (
+            <motion.div
+              initial={{ height: 0, opacity: 0 }}
+              animate={{ height: 'auto', opacity: 1 }}
+              exit={{ height: 0, opacity: 0 }}
+              transition={{ duration: 0.3 }}
+              style={{ overflow: 'hidden' }}
+            >
+              <motion.p className='text-white mb-4 min-h-40'>
+                {item.desc}
+              </motion.p>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
-      {isExpanded && <p className='text-white mb-4'>{item.desc}</p>}
-    </li>
+      {/* Apply a top margin to the checkbox instead of vertical centering */}
+      <input type='checkbox' className='mt-[2rem] -ml-16 checkbox-input' />
+    </motion.li>
   );
 };
 
