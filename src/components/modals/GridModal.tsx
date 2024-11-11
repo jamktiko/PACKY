027eb180c2d202ee0f01273eeff5ac0c2e961a51
unknown <augustboesen@gmail.com>
2024-11-11 10:13:02 +0200

    fixing the grid

--- src/components/modals/GridModal.tsx
+++ src/components/modals/GridModal.tsx
@@ -3,7 +3,10 @@ import { toggleModal } from '@/redux/reducers/gridModalReducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, type RootState } from '@/redux/store/store';
 import { CollectionData } from '@/utils/collectionData';
-import { setActiveCells } from '@/redux/reducers/gridStateReducer';
+import {
+  setActiveCells,
+  setChoosableCells,
+} from '@/redux/reducers/gridStateReducer';
 import { updateChoosableCells } from '@/utils/grid/updateGridState';
 import { motion } from 'framer-motion';
 
@@ -20,6 +23,9 @@ const GridModal = () => {
   const selectedCell = useSelector(
     (state: RootState) => state.gridStateReducer.selectedCell
   );
+  const choosableCells = useSelector(
+    (state: RootState) => state.gridStateReducer.choosableCells
+  );
   const gridmodal = useSelector(
     (state: RootState) => state.gridModalReducer.value
   );
@@ -31,7 +37,9 @@ const GridModal = () => {
     : [];
 
   const handleClick = (item: CollectionData, description: string) => {
-    if (activeCells.some((cell) => cell.id === item.name)) {
+    if (
+      activeCells.some((cell) => cell.id === item.name && cell.id !== 'Choose')
+    ) {
       console.log(`Ominaisuus ${item.name} on jo valittu.`);
     } else {
       dispatch(
@@ -55,49 +63,65 @@ const GridModal = () => {
     }
   };
 
+  const handleEmptyCell = () => {
+    if (selectedCell) {
+      console.log('selectedCell', selectedCell);
+      const cellIndex = activeCells.findIndex(
+        (cell) => cell.row === selectedCell.row && cell.col === selectedCell.col
+      );
+      console.log('cellIndex', cellIndex);
+      if (cellIndex !== -1) {
+        const updatedActiveCells = [...activeCells];
+        updatedActiveCells.splice(cellIndex, 1);
+        dispatch(setActiveCells(updatedActiveCells));
+        dispatch(setChoosableCells([...choosableCells, selectedCell]));
+        dispatch(toggleModal(false));
+      }
+    }
+  };
+  const isEmpty = true;
+
   return (
     <>
       {gridmodal && (
         <motion.div
           initial={{ opacity: 0, y: 200 }}
           animate={{ opacity: 1, y: 0 }}
-          className="grid-modal"
+          className='grid-modal'
         >
-          <button
-            className="grid-modal-item"
-            onClick={() =>
-              handleClick(
-                {
-                  name: 'Choose',
-                  desc: '',
-                  weight: 0,
-                },
-                ''
-              )
-            }
-          >
-            <h1 className="font-bold">Empty Cell</h1>
-          </button>
           {gridmodalData.map((item, id) => {
             const isActive = activeCells.some((cell) => cell.id === item.name);
+            console.log('id', id);
             return (
-              <button
-                key={id}
-                className={`grid-modal-item ${isActive ? 'active' : ''}`}
-                onClick={() => handleClick(item, item.desc)}
-                disabled={isActive}
-              >
-                <h1 className="font-bold">{item.name}</h1>
-                <p>{item.desc}</p>
-              </button>
+              <>
+                {id === 0 && isActive === true && (
+                  <button
+                    className={`grid-modal-item col-span-2 ${
+                      isEmpty ? 'isEmpty' : ''
+                    }`}
+                    onClick={() => handleEmptyCell()}
+                  >
+                    <h1 className='font-bold'>Empty Cell</h1>
+                  </button>
+                )}
+                <button
+                  key={id}
+                  className={`grid-modal-item ${isActive ? 'active' : ''}`}
+                  onClick={() => handleClick(item, item.desc)}
+                  disabled={isActive}
+                >
+                  <h1 className='font-bold'>{item.name}</h1>
+                  <p>{item.desc}</p>
+                </button>
+              </>
             );
           })}
-          <div className="modal-header">
+          <div className='modal-header'>
             <h1>Choose feature</h1>
             <button
-              className="modal-toggle"
+              className='modal-toggle'
               onClick={() => dispatch(toggleModal(false))}
-              type="button"
+              type='button'
             >
               ⏎
             </button>
