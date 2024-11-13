c535653ee053bc3643573802d8d66d84f21d7ed8
unknown <augustboesen@gmail.com>
2024-11-13 09:30:50 +0200

    fixed responsivity on deselect grid feature

--- src/components/modals/OutputModal.tsx
+++ src/components/modals/OutputModal.tsx
@@ -32,7 +32,7 @@ const OutputModal = () => {
 
   if (isLoading) {
     return (
-      <div className="slider-container outputmodal-loader-center">
+      <div className='slider-container outputmodal-loader-center'>
         <Loader />
       </div>
     );
@@ -44,14 +44,12 @@ const OutputModal = () => {
         <motion.div
           initial={{ opacity: 0, y: 200 }}
           animate={{ opacity: 1, y: 0 }}
-          className="slider-container output-container"
+          className='slider-container output-container'
         >
-          <Slider {...settings} className="carousel">
+          <Slider {...settings} className='carousel'>
             {technologyGroups.map((group, index) => (
-              <div className="carousel-item" key={index}>
-                <h3 className="output-option-header">
-                  Stack Option {index + 1}
-                </h3>
+              <div className='carousel-item' key={index}>
+                <h3 className='output-option-header'>Option {index + 1}</h3>
                 {Object.entries(group).map(([category, techs]) => {
                   // Check if techs is an array or single technology
                   const techArray = Array.isArray(techs) ? techs : [techs];
@@ -59,14 +57,14 @@ const OutputModal = () => {
                   return (
                     <div
                       key={category}
-                      className="text-left md:ml-[20%] ml-[10%] mt-6"
+                      className='text-left md:ml-[20%] ml-[10%] mt-6'
                     >
-                      <p className="pl-3 pt-1 font-bold text-xl border-t border-l border-teal-500">
+                      <p className='pl-3 pt-1 font-bold text-xl border-t border-l border-teal-500'>
                         {category}
                       </p>
                       <ul>
                         {techArray.map((tech, i) => (
-                          <li className="md:text-base text-sm" key={i}>
+                          <li className='md:text-base text-sm' key={i}>
                             <b>Technology:</b> {tech.technology}
                             <p>Total Weight: {tech.totalWeight}</p>
                           </li>
