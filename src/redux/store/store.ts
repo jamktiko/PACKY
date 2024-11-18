d1f074a39d08baed8b9131cbf6e065344238fa7e
Petri Paasila <ad1782@student.jamk.fi>
2024-11-18 10:27:30 +0200

    modified store middleware back and deleted rect from search.tsx

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -57,7 +57,7 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        ignoredActions: [FLUSH, PAUSE, REHYDRATE, PERSIST, PURGE, REGISTER],
       },
     }),
 });
