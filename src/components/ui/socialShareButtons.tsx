7adf62acd143ce9c3a4c4ad87492595d65950030
unknown <augustboesen@gmail.com>
2024-11-20 13:06:46 +0200

    styled about page media buttons

--- src/components/ui/socialShareButtons.tsx
+++ src/components/ui/socialShareButtons.tsx
@@ -4,12 +4,18 @@ import { InstagramIcon, GithubIcon } from 'next-share';
 
 export default function SocialShareButtons() {
   return (
-    <div className="flex gap-2">
-      <a href="https://www.instagram.com/packy_ticorporate/">
-        <InstagramIcon size="5rem" round={true} />
+    <div className='flex gap-2 absolute left-0 mt-6 w-screen justify-evenly'>
+      <a
+        className='hover:text-white rounded-full hover:bg-white bg-none'
+        href='https://www.instagram.com/packy_ticorporate/'
+      >
+        <InstagramIcon size='5rem' round={true} />
       </a>
-      <a href="https://github.com/jamktiko/PACKY">
-        <GithubIcon size="5rem" round={true} />
+      <a
+        className='hover:text-white rounded-full hover:bg-white bg-none'
+        href='https://github.com/jamktiko/PACKY'
+      >
+        <GithubIcon size='5rem' round={true} />
       </a>
     </div>
   );
