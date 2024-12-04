e99878435c7a1baa269306e240662dd74872efd9
unknown <augustboesen@gmail.com>
2024-12-04 09:54:37 +0200

    transition animations to stackbuilder parts now llook 10000% cooler

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { AppDispatch, RootState } from '@/redux/store/store';
 import { fetchLibrary } from '@/redux/reducers/libraryDataReducer';
 import { useSelector, useDispatch } from 'react-redux';
-import { motion } from 'framer-motion';
+import { delay, motion } from 'framer-motion';
 import ExpandableItem from '@/components/buttons/ExpandField';
 import Loader from '@/components/ui/loader';
 import { useFetchCollections } from '@/hooks/useFetchCollections';
@@ -34,25 +34,6 @@ const SearchBar = () => {
 
   const [isLibraryVisible, setIsLibraryVisible] = useState(false);
 
-  // useEffect(() => {
-  //   if (!isLibraryVisible) {
-  //     setIsLibraryVisible(true);
-  //   }
-  // }, [isLibraryVisible]);
-
-  // useEffect(() => {
-  //   if (isLibraryVisible) {
-  //     const gridButtonElement = document.getElementById('library');
-  //     if (gridButtonElement) {
-  //       window.scrollTo({
-  //         top: Math.abs(0),
-  //         left: Math.abs(0),
-  //         behavior: 'smooth',
-  //       });
-  //     }
-  //   }
-  // }, [isLibraryVisible]);
-
   // useEffect for handling library visibility, tutorialmodal visibility and fetching data
   useEffect(() => {
     dispatch(fetchLibrary());
@@ -80,18 +61,18 @@ const SearchBar = () => {
   }, [dispatch, tutorialModalState, isLibraryVisible]);
 
   return (
-    <div>
-      <h1 className="text-center text-2xl ml-16">
+    <div className='md:flex flex-col origin-center items-center'>
+      <h1 className='text-center text-2xl ml-16'>
         Optional: Choose familiar technologies
       </h1>
       {/* Search box to filter by name */}
       <input
         style={{ color: 'gray', backgroundColor: 'black' }}
-        type="text"
-        placeholder="Search for technologies"
+        type='text'
+        placeholder='Search for technologies'
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
-        className="library-search"
+        className='library-search'
         disabled={isTutorialModalOpen}
       />
       {/* Display filtered data or loader */}
@@ -101,11 +82,18 @@ const SearchBar = () => {
         <motion.ul
           initial={{ opacity: 0, x: 200 }}
           animate={{ opacity: 1, x: 0 }}
-          className="mt-4"
-          id="library"
+          className='mt-4'
+          id='library'
         >
           {filteredlibraryData.map((item, index) => (
-            <ExpandableItem key={index} item={item} />
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ delay: index * 0.075 }}
+            >
+              <ExpandableItem key={index} item={item} />
+            </motion.div>
           ))}
         </motion.ul>
       )}
